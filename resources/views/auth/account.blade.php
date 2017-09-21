d96a9f9847b78c0915bc3b9fd4689ed556cddf09
Idiakose Sunday <idiakosesunday@gmail.com>
2017-09-21 01:09:09 +0100

    CURL ssl error fixed by downloading .pem file

--- resources/views/auth/account.blade.php
+++ resources/views/auth/account.blade.php
@@ -6,6 +6,10 @@
 
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
+{{-- 
+			Debugging ssl issue with guzzlehttp
+			{!! var_dump(openssl_get_cert_locations()) !!}
+			 --}}
 			<img src="{{ route('filefetch', ['filename' => $user->avatar, 'admin' => 1]) }}" alt="User Profile image">
 			<h2>Profile</h2>
 			<form action="{{ route('account.update') }}" enctype="multipart/form-data" method="POST">
